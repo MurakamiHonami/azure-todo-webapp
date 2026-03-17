f0a2746e22ed089141e7c2d15076182b3876c792
MurakamiHonami <mnv2525@gmail.com>
2026-03-17 01:35:37 +0000

    add appservice

--- infra/webapp.tf
+++ infra/webapp.tf
@@ -5,3 +5,33 @@ resource "azurerm_service_plan" "asp" {
   os_type             = "Linux"
   sku_name            = "F1"
 }
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "${var.project}-${var.environment}-app-tama-111"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.asp.id
+  site_config {
+    always_on = false
+    application_stack {
+      docker_image_name        = var.container
+      docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
+      docker_registry_username = azurerm_container_registry.acr.admin_username
+      docker_registry_password = azurerm_container_registry.acr.admin_password
+    }
+  }
+  app_settings = {
+    "MYSQL_HOST"     = "host"
+    "MYSQL_PORT"     = "3306"
+    "MYSQL_USERNAME" = var.mysql_username
+    "MYSQL_PASSWORD" = var.mysql_password
+    "MYSQL_DATABASE" = var.mysql_database
+    "MYSQL_SSL"      = "true"
+  }
+  lifecycle {
+    ignore_changes = [
+      site_config[0].application_stack,
+    ]
+  }
+
+}
